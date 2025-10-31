e4e22fc1c96f6a77839c26084470e5fcc45f83b9
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2025-10-31 20:10:00 +0530

    create dto

---
+++ src/main/java/com/example/ead_backend/dto/vehicle/VehicleDTO.java
@@ -0,0 +1,18 @@
+package com.example.ead_backend.dto.vehicle;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VehicleDTO {
+    private Long id;
+    private String model;
+    private String color;
+    private String licensePlate;
+    private int year;
+    private LocalDate registrationDate;
+}
